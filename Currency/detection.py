79fb43fe7cd7f7295f250b158b06bae1bf86d5a9
Arav Sachdeva <aravsachdeva@gmail.com>
2021-12-15 01:31:54 -0800

    commit #2

--- Currency/detection.py
+++ Currency/detection.py
@@ -1,6 +1,9 @@
 import keras_ocr
 import os
 from PIL import Image
+from gtts import gTTS
+from playsound import playsound
+
 
 
 pipeline = keras_ocr.pipeline.Pipeline()
@@ -8,10 +11,8 @@ pipeline = keras_ocr.pipeline.Pipeline()
 directory = os.getcwd()
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
-img = Image.open(os.path.join(PROJECT_PATH,"Crop100.png")).convert('L')
+img = Image.open(os.path.join(PROJECT_PATH,"100Dollars.png")).convert('L')
 img.save(os.path.join(PROJECT_PATH,'greyscale.png'))
-
-print(hello)
 
 images = [
     keras_ocr.tools.read(os.path.join(PROJECT_PATH,"greyscale.png"))     
@@ -20,5 +21,45 @@ images = [
 
 prediction_groups = pipeline.recognize(images)
 
-print(prediction_groups)
+washington_1 = "wash*"
+lincoln_5 = "lincole"
+hamilton_10 = "hamil"
+jackson_20 = "jacks"
+print('Lenght us: ', len(prediction_groups[0]))
+for i in range(len(prediction_groups[0])):
+    result = [ m[0] for m in [l[i] for l in prediction_groups]]
+    print(result)
+    #if(washington_1 in result):
+    if(result[0].startswith('wash')):
+        print(result)
+        obj = gTTS(text='1 DOLLAR BILL', lang='en', slow=False)
+        obj.save('/tmp/currency.mp3')
+        playsound('/tmp/currency.mp3')
+    elif(result[0].startswith('linc')):
+        print(result)
+        obj = gTTS(text='5 DOLLAR BILL', lang='en', slow=False)
+        obj.save('/tmp/currency.mp3')
+        playsound('/tmp/currency.mp3')
+    elif(result[0].startswith('hami')):
+        print(result)
+        obj = gTTS(text='10 DOLLAR BILL', lang='en', slow=False)
+        obj.save('/tmp/currency.mp3')
+        playsound('/tmp/currency.mp3')
+    elif(result[0].startswith('jacks')):
+        print(result)
+        obj = gTTS(text='20 DOLLAR BILL', lang='en', slow=False)
+        obj.save('/tmp/currency.mp3')
+        playsound('/tmp/currency.mp3')
+    elif(result[0].startswith('gran')):
+        print(result)
+        obj = gTTS(text='50 DOLLAR BILL', lang='en', slow=False)
+        obj.save('/tmp/currency.mp3')
+        playsound('/tmp/currency.mp3')
+    elif(result[0].startswith('fran')):
+        print(result)
+        obj = gTTS(text='100 DOLLAR BILL', lang='en', slow=False)
+        obj.save('/tmp/currency.mp3')
+        playsound('/tmp/currency.mp3')
+
+
 
--- manage.py
+++ manage.py
@@ -18,5 +18,27 @@ def main():
     execute_from_command_line(sys.argv)
 
 
+# def take_picture():
+#     videoCaptureObject = cv2.VideoCapture(0)
+#     while(videoCaptureObject.isOpened()):
+#         ret,frame = videoCaptureObject.read()
+#         cv2.imshow('Capturing Video',frame)
+#         cv2.imwrite('NewPicture.jpg',frame)
+#         key=cv2.waitKey(10)
+#         if key == ord('q'):
+#             break
+#     videoCaptureObject.release()
+#     cv2.destroyAllWindows()
+#     return
+
+# def take_photo():
+#     tkWindow = Tk()
+#     tkWindow.geometry('250x350')
+#     tkWindow.title('Take Photo of the currency')
+
+#     button = Button(tkWindow,text = 'Click',command = take_picture)
+#     button.pack()
+#     tkWindow.mainloop()
+
 if __name__ == '__main__':
     main()
